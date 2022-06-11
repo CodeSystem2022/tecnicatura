d8516a0b9fe63493fa004475aa3c71dd0e620ee3
mauricio sandrea <mauriciosandrea2019@gmail.com>
2022-06-11 15:17:26 -0300

    lecciones 1 de java

---
+++ java/leccion 1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,30 @@
+
+
+/** 
+ *
+ * @author mauri
+ */
+public class HolaMundo {
+    public static void main( String args []){
+    System.out.println("hola mundo desde java");
+    
+    //tipo entero
+    int miVariable = 10;
+    System.out.println(miVariable);
+    
+    miVariable = 5 ;
+    System.out.println(miVariable);
+    
+    //tipo string
+    
+    String miVariableCadena = "bienvenidos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "sigamos creciendo en programacion";
+        System.out.println(miVariableCadena);
+           
+         
+        
+    
+    }
+}
---
+++ java/leccion 1/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,18 @@
+
+
+package holamundo;
+
+/**
+ *
+ * @author mauri
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
