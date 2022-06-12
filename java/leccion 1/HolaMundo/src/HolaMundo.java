a785adee23822e312d3476828f954e45de141624
mauricio sandrea <mauriciosandrea2019@gmail.com>
2022-06-12 14:16:30 -0300

    inferencia de datos operadires de asignacion

--- java/leccion 1/HolaMundo/src/HolaMundo.java
+++ java/leccion 1/HolaMundo/src/HolaMundo.java
@@ -78,9 +78,44 @@ public class HolaMundo {
     byte numEnteroByte = 127;
     System.out.println("numEnteroByte - " + numEnteroByte);
     System.out.println("valor minimo del byte:" + Byte.MIN_VALUE);
-   // System.out.println("Valor maximodel byte" + Byte.MAX_VALUE )
+    System.out.println("Valor maximodel byte" + Byte.MAX_VALUE );
+    
+    /**
+    Short numEnteroShort = (short)32768;  // si pongo un numero mayor da error    //si agrego el (short) no salta error pero pierde presicion y da negativo
+    Short numEnteroShort = 32767;
+    System.out.println("numEnteroShort - " + numEnteroShort);
+    System.out.println("valor minimo del Short:" + Short.MIN_VALUE);
+    System.out.println("Valor maximodel short" + Short.MAX_VALUE);
+    */
+    
+    //int numEnteroInt = (int)2147483647L; (no salta error pero lo muestra en negativo debo agregar la L para que no salte a demas del int
+    int numEnteroInt = 10;
+    System.out.println("numEnteroInt - " + numEnteroInt);
+    System.out.println("valor minimo del Int:" + Integer.MIN_VALUE);
+    System.out.println("Valor maximodel Int" + Integer.MAX_VALUE);
     
     
+    long numEnteroLong = 9223372036854775807L; //Es el maximo entero (agrego L para que no de error), si quiero un numer fmayor se usan n flotantes
+    System.out.println("numEnteroLong - " + numEnteroLong);
+    System.out.println("valor minimo del Long:" + Long.MIN_VALUE);
+    System.out.println("Valor maximodel Long" + Long.MAX_VALUE);
+    
+    float numFloat = 10.0F; //usar mayusculas para mejor comprension, minuscula acepta pero confunde
+    //si ponemos un decimal, hay que agregar F para que indique que la literal es float
+    // otra forma es indicar al compilador que lo tome  com flotante poninedo (float)
+    // se pueden usar ambos o uno solo (float)10F;
+    //float numFloat = (float)3.4028236E38D;
+    // Si uso un num mayor da error se arregla (float) + D
+    System.out.println("numFloat - " + numFloat);
+    System.out.println("valor minimo del Float: " + Float.MIN_VALUE);
+    System.out.println("Valor maximodel Float: " + Float.MAX_VALUE);
+    
+    
+    
+    double numDouble = 10;
+    System.out.println("num - " + numDouble);
+    System.out.println("valor minimo del Double: " + Double.MIN_VALUE);
+    System.out.println("Valor maximodel Double: " + Double.MAX_VALUE);
         
     
     }
